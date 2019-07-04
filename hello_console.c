b76477442618bfdf05762383d353ad0002936681
MeowDada <jackchang@gmail.com>
2019-07-04 12:23:45 +0800

    Basic C console template

---
+++ hello_console.c
@@ -0,0 +1,107 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <unistd.h>
+
+int continueous_mode = 0;
+
+void enable_command_mode(void)
+{
+    continueous_mode = 1;
+}
+
+void guess_lucky_number(int guess)
+{
+    srand(time(NULL));
+    int lucky_number = (rand() % 3) + 1;
+    if (guess==lucky_number)
+        fprintf(stdout, "BINGO!!!\n");
+    else
+        fprintf(stdout, "Nice tried!!!\n");
+}
+
+void roll_dice(int num_rolls)
+{
+    srand(time(NULL));
+    while (num_rolls--) {
+        int value = (rand() % 6) + 1;
+        fprintf(stdout, "You roll to %d !!!\n", value);
+    }
+}
+
+void show_program_name(const char *program_name)
+{
+    fprintf(stdout, "%s\n", program_name);
+}
+
+void say_hello(void)
+{
+    fprintf(stdout, "Hello World!\n");
+}
+
+void usage(const char *program_name)
+{
+    fprintf(stderr, "Usage: [-r<num_rolls>] [-g<guess_number>]\n");
+    fprintf(stderr, "Where:\n");
+    fprintf(stderr, "   -g - Guess a lucky number (1-3)\n");
+    fprintf(stderr, "   -c - Enable continueous command mode\n");
+    fprintf(stderr, "   -r - Roll a dice and show its value\n");
+    fprintf(stderr, "   -S - Show the program name\n");
+    fprintf(stderr, "   -s - Say Hello to this world\n");
+    fprintf(stderr, "   -? - This help text\n");
+}
+
+int main(int argc, char **argv) 
+{
+    const char *program_name;
+    int opt;
+
+    program_name = argv[0];
+
+    /* 
+     * Parse startup command by using build-in function getopt(...)
+     */
+    while ((opt = getopt(argc, argv, "g:cr::Ss")) != -1) {
+        switch (opt) {
+            case 'g':
+                guess_lucky_number(atoi(optarg));
+                break;
+            case 'c':
+                enable_command_mode();
+                break;
+            case 'r':
+                if (optarg) {
+                    fprintf(stderr, "option = %s\n", optarg);
+                    int num_rolls = atoi(optarg);
+                    fprintf(stderr, "Roll dice for %d times !!!\n", num_rolls);
+                    roll_dice(num_rolls);
+                }
+                else 
+                    roll_dice(1);
+                break;
+            case 'S':
+                show_program_name(program_name);
+                break;
+            case 's':
+                say_hello();
+                break;
+            case 'h':
+            case '?':
+            default:
+                usage(program_name);
+                break;
+        }
+    }
+
+    /*
+     * Handle those options which are not being handle by getopt()
+     */
+    argc -= optind;
+    argv += optind;
+
+    if (continueous_mode) {
+        
+    }
+
+    return 0;
+}
